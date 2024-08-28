b166a04f596d8db24974802e85eb7c5b5d6b121d
David-19960111 <david11011996@gmail.com>
2024-08-28 16:35:04 -0300

    terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "Variable region aws"
+  type        = string
+  default     = false
+}
+
+variable "vpc_cidr" {
+  description = "Variable CIDR for vpc"
+  type        = string
+  default     = false
+}
