6bdd1dd38ce07e28e0a5b83b9e5056d254a34d82
David-19960111 <david11011996@gmail.com>
2024-09-11 13:47:47 -0300

    delete iac

--- terraform/ecr/main.tf
+++
@@ -1,7 +0,0 @@
-module "ecr" {
-  source = "../modules/ecr"
-
-  mutability = var.mutability
-  ecr_repo_name_one = var.ecr_repo_name_one
-  ecr_repo_name_two = var.ecr_repo_name_two
-}
--- terraform/modules/ecr/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repo_url_one" {
-  value = aws_ecr_repository.frontend.repository_url
-}
-
-output "ecr_repo_url_two" {
-  value = aws_ecr_repository.backend.repository_url
-}
